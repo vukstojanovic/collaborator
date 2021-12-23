ecc5effcf38b21f4dff835b321ac7c6f8c7fc6d6
vukstojanovic <82164509+vukstojanovic@users.noreply.github.com>
2021-12-23 16:59:06 +0100

    Check husky

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,19 @@
-import React from 'react'
+import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { add, sub } from '../redux/actions/counter';
 import { RootState } from '../redux/reducers';
 
 function Home() {
     const dispatch = useDispatch();
-  const counter = useSelector((state: RootState) => state.count.countSum);
-  console.log(counter);
+    const counter = useSelector((state: RootState) => state.count.countSum);
+    console.log(counter);
     return (
         <div>
-        <button onClick={() => dispatch(sub())}>-1</button>
-        <div>{counter}</div>
-        <button onClick={() => dispatch(add())}>+1</button>
-      </div>
-    )
+            <button onClick={() => dispatch(sub())}>-1</button>
+            <div>{counter}</div>
+            <button onClick={() => dispatch(add())}>+1</button>
+        </div>
+    );
 }
 
-export default Home
+export default Home;
