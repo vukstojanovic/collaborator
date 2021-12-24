4c919a076a9adb7ece3b9444b0b2cad22e95eb3e
SreckoStojic <stojic.srecko@gmail.com>
2021-12-24 14:38:52 +0100

    Add new lines on App.tsx to check formatting.

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,18 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import About from './pages/About';
 import Home from './pages/Home';
 
 function App() {
-  
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/about" element={<About />}/>
-      </Routes>
-    </BrowserRouter>
-    
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
