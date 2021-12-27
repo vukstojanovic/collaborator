8c1971b78ab8a9f3d9b9200baad2d70c6d3ac81f
SreckoStojic <stojic.srecko@gmail.com>
2021-12-27 13:50:29 +0100

    [CLB-7] Add font awesome dependecies and icon example in about page.

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,6 +1,10 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+
 function About() {
     return (
         <div>
+            <FontAwesomeIcon icon={faCoffee} />
             <h2>hello Automat</h2>
             <h1>About Page</h1>
         </div>
