0b050df21e8ce31bf38799f973db01467abacbca
Zola987 <laza.ivosevic87@gmail.com>
2021-12-29 11:08:04 +0100

    Changes to Modal commited

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import ModalCSS from './ModalCSS.module.css';
+import styles from './ModalCSS.module.css';
 
 const Modal = () => {
     return ReactDom.createPortal(
-        <div className={ModalCSS.modal_container}>
-            <div className={ModalCSS.modal}>
-                <header className={ModalCSS.modal_header}>
-                    <h2 className={ModalCSS.title}>Add new skill:</h2>
+        <div className={styles.modal_container}>
+            <div className={styles.modal}>
+                <header className={styles.modal_header}>
+                    <h2 className={styles.title}>Add new skill:</h2>
                 </header>
                 <input type="text" />
-                <footer className={ModalCSS.modal_footer}>
-                    <button className={ModalCSS.modal_discard}>Discard</button>
-                    <button className={ModalCSS.modal_add}>Add</button>
+                <footer className={styles.modal_footer}>
+                    <button className={styles.modal_discard}>Discard</button>
+                    <button className={styles.modal_add}>Add</button>
                 </footer>
             </div>
         </div>,
