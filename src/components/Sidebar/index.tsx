ec882bb87e4b2b97ac8c56c3282bfc3218367146
vukstojanovic <vuks838@gmail.com>
2021-12-29 13:47:13 +0100

    [CLB-9] Add generic items for sidebar and sidebarArray

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,10 +1,5 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faIdCard } from '@fortawesome/free-solid-svg-icons';
-import { faClock } from '@fortawesome/free-solid-svg-icons';
-import { faUsers } from '@fortawesome/free-solid-svg-icons';
-import { faProjectDiagram } from '@fortawesome/free-solid-svg-icons';
-import { faChartLine } from '@fortawesome/free-solid-svg-icons';
-import { faStreetView } from '@fortawesome/free-solid-svg-icons';
+import sidebarData from './SidebarData';
 import styles from './Sidebar.module.css';
 
 const Sidebar = () => {
@@ -15,39 +10,18 @@ const Sidebar = () => {
             </div>
             <p className={styles.fullname}>fullname</p>
             <ul className={styles['sidebar-fields']}>
-                <li>
-                    <FontAwesomeIcon
-                        className={styles.icon}
-                        icon={faStreetView}
-                    />
-                    <span>overview</span>
-                </li>
-                <li>
-                    <FontAwesomeIcon className={styles.icon} icon={faIdCard} />
-                    <span>profile</span>
-                </li>
-                <li>
-                    <FontAwesomeIcon className={styles.icon} icon={faClock} />
-                    <span>timesheet</span>
-                </li>
-                <li>
-                    <FontAwesomeIcon className={styles.icon} icon={faUsers} />
-                    <span>people</span>
-                </li>
-                <li>
-                    <FontAwesomeIcon
-                        className={styles.icon}
-                        icon={faProjectDiagram}
-                    />
-                    <span>projects</span>
-                </li>
-                <li>
-                    <FontAwesomeIcon
-                        className={styles.icon}
-                        icon={faChartLine}
-                    />
-                    <span>stats</span>
-                </li>
+                {sidebarData.map((sidebarItem) => {
+                    const { id, icon, label } = sidebarItem;
+                    return (
+                        <li key={id}>
+                            <FontAwesomeIcon
+                                className={styles.icon}
+                                icon={icon}
+                            />
+                            <span>{label}</span>
+                        </li>
+                    );
+                })}
             </ul>
         </aside>
     );
