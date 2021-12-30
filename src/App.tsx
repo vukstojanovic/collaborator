ffc7a9eb21bf4521b0ef04a981fb1c17c84fc8c2
Kim Novak <kimnovak187@gmail.com>
2021-12-30 08:37:58 +0100

    Remove duplicate import for testBB

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import About from './pages/About';
 import Home from './pages/Home';
 import TestBB from './pages/TestBB';
 import TestSS from './pages/TestSS';
-import TetstBB from './pages/TetstBB';
 import TestVS from './pages/TestVS';
 import Header from './components/Header/Header';
 
@@ -16,7 +15,6 @@ function App() {
                 <Route path="/header" element={<Header />} />
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
-                <Route path="/testbb" element={<TetstBB />} />
                 <Route path="/testvs" element={<TestVS />} />
                 <Route path="/testbb" element={<TestBB />} />
                 <Route path="/testss" element={<TestSS />} />
