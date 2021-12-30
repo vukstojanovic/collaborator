e0d43a652cfce8be410e36dcf701f9a16b2c7abd
Stefan Meza <60515957+stefanmeza94@users.noreply.github.com>
2021-12-30 10:26:28 +0100

    [CLB-15] Add localization to project! (#13)
    * Add package.lock.json files to stage
    
    * Add localization to CLB-15

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Home from './pages/Home';
 import TestBB from './pages/TestBB';
 import TestSS from './pages/TestSS';
 import TestVS from './pages/TestVS';
+import TestSM from './pages/TestSM';
 import Header from './components/Header/Header';
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
                 <Route path="/header" element={<Header />} />
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
+                <Route path="/testsm" element={<TestSM />} />
                 <Route path="/testvs" element={<TestVS />} />
                 <Route path="/testbb" element={<TestBB />} />
                 <Route path="/testss" element={<TestSS />} />
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
@@ -7,11 +7,15 @@ import store from './redux/store/store';
 import { Provider } from 'react-redux';
 import './theme.module.css';
 
+import './localization/i18n';
+
 ReactDOM.render(
     <React.StrictMode>
-        <Provider store={store}>
-            <App />
-        </Provider>
+        <Suspense fallback={<div>Loading...</div>}>
+            <Provider store={store}>
+                <App />
+            </Provider>
+        </Suspense>
     </React.StrictMode>,
     document.getElementById('root')
 );
